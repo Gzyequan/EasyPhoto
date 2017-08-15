1e682f7af6d00f9a4a3b499d4946a984d25724cb
Gzyequan <673541964@qq.com>
2017-08-15 08:37:28 +0800

    添加LauncherActivity背景图片动画，优化fade_action.xml与hold_action.xml动画时间

--- app/src/main/java/com/yequan/easyphoto/LauncherActivity.java
+++ app/src/main/java/com/yequan/easyphoto/LauncherActivity.java
@@ -4,14 +4,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.yequan.easyphoto.utils.PreferencesUtil;
 
-public class LaunchActivity extends BaseActivity {
+public class LauncherActivity extends BaseActivity {
 
+    private ImageView launcherPhoto;
     private TextView mTimer;
-    private int count = 2;
+    private int count = 3;
     private boolean is_first_launch;
     private Intent intent;
 
@@ -19,6 +23,7 @@ public class LaunchActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         is_first_launch = PreferencesUtil.getPreferences().getBoolean(this, "IS_FIRST_LAUNCH", true);
+        launcherPhotoAnimation();
         handler.sendEmptyMessageDelayed(0, 1000);
     }
 
@@ -29,17 +34,24 @@ public class LaunchActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        launcherPhoto = (ImageView) findViewById(R.id.ep_launcher_background);
         mTimer = (TextView) findViewById(R.id.ep_launcher_timer);
         mTimer.setText(count + "s");
+    }
+
+    private void launcherPhotoAnimation() {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.launcher_photo_animation);
+        launcherPhoto.setAnimation(animation);
+        animation.start();
     }
 
     private int getCount() {
         count--;
         if (count == 0) {
             if (is_first_launch) {
-                intent = new Intent(LaunchActivity.this, SplashActivity.class);
+                intent = new Intent(LauncherActivity.this, SplashActivity.class);
             } else {
-                intent = new Intent(LaunchActivity.this, MainActivity.class);
+                intent = new Intent(LauncherActivity.this, MainActivity.class);
             }
             startActivity(intent);
             finish();
